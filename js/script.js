7ab02ebbd5a9512e590cff03c401c4b3bfa0209b
naveen-98 <naveemayantha@gmail.com>
2022-10-17 12:32:26 +0530

    main page index.html

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//typing animation
+var typed = new Typed(".typing",{
+    strings:[" ","Web Developer","Cyber Security Engineer","Penetration Tester","Blogger"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
+
+/* Aside */
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("back-section");
+            }
+
+            for(let j=0; j<totalNavList; j++)
+            {
+                if(navList[j].querySelector("a").classList.contains("active"))
+                {
+                    allSection[i].classList.add("bacl-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+        })
+    }
+    function showSection(element)
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#"+ target).classList.add("active")
+    }
